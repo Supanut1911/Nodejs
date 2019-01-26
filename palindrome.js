98ad33faf8f93661a197c4581167cc827ab93853
supanut1911 <ynotmela@gmail.com>
2019-01-26 17:44:46 +0700

    fixStandardInput_Bug

--- palindrome.js
+++ palindrome.js
@@ -1,70 +1,50 @@
-// var str = d
-// // str = stdin
-// var str2 = ""
-// console.log(str);
-// for(var i = str.length-1,j = 0    ; i >= 0  ;i--, j++){
-//
-//        str2 = str2 + str[i]
-// }
-//
-// if(str == str2){
-//   console.log("yes , it is palindrome");
-// }
-// else{
-//   console.log("no");
-// }
+//-----------------------------[STYLE_1]----------------------------------------
+
 console.log("enter string:");
-let stdin = process.openStdin()
-stdin.addListener("data", (d) => {
+  let stdin = process.openStdin()
+  stdin.addListener("data", (d) => {
+  var str = d.toString().trim()
+  var status = true
 
- var str = d.toString().trim()
- var str2 = ""
- str = str.toLowerCase()
- for(var i = str.length-1,j = 0    ; i >= 0  ;i--, j++){
+  for(var i =0;i<str.length;i++){
+    if(str[i] == str[str.length-1-i]){
+      status = true;
+    }
+    else{
+      status = false;
+      break;
+    }
+  }
 
-        str2 = str2 + str[i]
- }
-
- if(str == str2){
-   console.log("yes , it is palindrome");
- }
- else{
-   console.log("no");
- }
-
- stdin.end()
+  if(status == true ){
+    console.log("yes it palindrome")
+  }
+  else{
+    console.log("no")
+  }
+  stdin.end()
 });
 
-
-let stdin = process.openStdin()
-
-var str = stdin
-
+//------------------------------[STYLE_2]---------------------------------------
+// console.log("enter string:");
+// let stdin = process.openStdin()
+// stdin.addListener("data", (d) => {
 //
+//   var str = d.toString().trim()
+//   var str2 = ""
+//   str = str.toLowerCase()
+//   for(var i = str.length-1,j = 0    ; i >= 0  ;i--, j++){
 //
+//     str2 = str2 + str[i]
+//   }
 //
-
-
-
-
-
-//------------------------------------------------------------------------------
-// var str = 'axba'
-// var status = true
-//
-// for(var i =0;i<str.length;i++){
-//   if(str[i] == str[str.length-1-i]){
-//     status = true;
+//   if(str == str2){
+//     console.log("yes , it is palindrome");
 //   }
 //   else{
-//     status = false;
-//     break;
+//     console.log("no");
 //   }
-// }
 //
-// if(status == true ){
-//   console.log("yes it palindrome")
-// }
-// else{
-//   console.log("no")
-// }
+//   stdin.end()
+// });
+//
