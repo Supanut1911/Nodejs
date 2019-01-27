a4871730a74444ca33e6a6ccc724bf7676a33501
supanut1911 <ynotmela@gmail.com>
2019-01-27 15:52:36 +0700

    arrow function is new life

---
+++ convertToArrowFunction.js
@@ -0,0 +1,39 @@
+//การเปลี่ยนเป้น arrow funciton ให้เอา คำว่า function ออก
+//แล้วใส่ => เพิ่มเข้าไปหลัง (parameter)
+
+
+//1.convert anonymous fucntion to anonymous Arrow function
+
+setTimeout(function(){
+  console.log();
+},2000)
+
+//change anonymous func to anonymous arrow func
+setTimeout( () => {
+  console.log("onChange");
+},2000)
+
+
+//2.covert curry function to curry arrow function
+function add(x){
+  return function(y){
+    return x + y
+  }
+}
+add1 = (x) => {
+  return (y) => {
+    return x + y
+  }
+}
+console.log(add1(77)(99));
+
+//--------------------
+// add2 = (x) => {
+//   return (y) => {
+//     return (z) => {
+//       return x + y +z
+//     }
+//   }
+// }
+//
+// console.log(add2(3)(4)(5));
